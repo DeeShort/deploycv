f6e81f3f52d93758a4531f368852e134120c83a1
deeshort <deedisp@yahoo.com>
2020-10-15 20:24:53 +0200

    Send local

--- src/main/java/io/d/coronavirustracker/services/CorVirDatService.java
+++ src/main/java/io/d/coronavirustracker/services/CorVirDatService.java
@@ -22,7 +22,7 @@ import java.util.List;
 @Service
 public class CorVirDatService {
 
-    private static String VIRUS_DATA_URL="https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
+    private static String VIRUS_DATA_URL="https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
     private List<LocationStates> allStats=new ArrayList<>();
 
